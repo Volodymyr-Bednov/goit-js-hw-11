37e9a5ece59e6e773fc249cebe6bdda61aba34f2
Volodymyr-Bednov <109081369+Volodymyr-Bednov@users.noreply.github.com>
2022-12-17 17:05:15 +0200

    preparation

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simple-lightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// API key: 32131448-3a2109ffe1ebf52f926fc3134
