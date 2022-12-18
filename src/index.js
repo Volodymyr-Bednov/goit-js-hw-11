5a3d31b3cb527171c5ca3b5df0f1c6c27d2fa4ab
Volodymyr-Bednov <109081369+Volodymyr-Bednov@users.noreply.github.com>
2022-12-18 02:31:20 +0200

    stage-2 html + js + css

--- src/index.js
+++ src/index.js
@@ -8,22 +8,47 @@ import { getImages } from './scripts/sendrequest';
 const formRef = document.querySelector('form#search-form');
 const findValue = document.querySelector('input[name="searchQuery"]');
 const galleryRef = document.querySelector('div.gallery');
+const loadMoreBtnRef = document.querySelector('button.load-more');
+
+const currentState = {
+  query: null,
+  page: 1,
+};
 
 formRef.addEventListener('submit', evt => {
   evt.preventDefault();
-  const query = findValue.value;
-  getImages(query).then(data => {
-    if (data.length == 0) {
+  currentState.query = findValue.value;
+  currentState.page = 1;
+  getData(currentState);
+
+  findValue.value = '';
+});
+
+const getData = currentState => {
+  getImages(currentState).then(data => {
+    if (data.totalHits == 0) {
       Notiflix.Notify.info(
         'Sorry, there are no images matching your search query. Please try again.'
       );
       return;
     }
-    console.log(data);
-    renderContent(data);
+    const totalPages = data.totalHits / data.hits.length;
+    console.log(currentState.page, Math.floor(totalPages));
+    hidenButton(totalPages);
+    renderContent(data.hits);
   });
-  findValue.value = '';
-});
+};
+
+const hidenButton = totalPages => {
+  if (currentState.page >= Math.floor(totalPages)) {
+    Notiflix.Notify.failure(
+      "We're sorry, but you've reached the end of search results."
+    );
+    loadMoreBtnRef.hidden = true;
+    return;
+  }
+  loadMoreBtnRef.hidden = false;
+};
 
 const renderContent = comment => {
   clearGallery();
@@ -44,19 +69,19 @@ const createContent = ({
 }) => {
   return `
     <div class="photo-card">
-        <a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
+        <a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy" class="item-image"/></a>
         <div class="info">
             <p class="info-item">
-            <b>${likes}</b>
+            <b>Likes:</b><span>${likes}</span>
             </p>
             <p class="info-item">
-            <b>${views}</b>
+            <b>Views:</b><span>${views}</span>
             </p>
             <p class="info-item">
-            <b>${comments}</b>
+            <b>Comments:</b><span>${comments}</span>
             </p>
             <p class="info-item">
-            <b>${downloads}</b>
+            <b>Downloads:</b><span>${downloads}</span>
             </p>
         </div>
     </div>`;
@@ -67,6 +92,11 @@ const clearGallery = () => {
     galleryRef.removeChild(galleryRef.lastChild);
   }
 };
+
+loadMoreBtnRef.addEventListener('click', evt => {
+  currentState.page += 1;
+  getData(currentState);
+});
 
 let lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
--- src/scripts/sendrequest.js
+++ src/scripts/sendrequest.js
@@ -1,17 +1,19 @@
 import Axios from 'axios';
 export { getImages };
 
-const getImages = async query => {
+const getImages = async ({ query, page }) => {
   const baseURL = 'https://pixabay.com/api/';
   return await Axios.get(baseURL, {
     params: {
       key: '32131448-3a2109ffe1ebf52f926fc3134',
       q: `${query}`,
+      page: page,
+      per_page: 40,
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: 'true',
     },
   })
-    .then(res => res.data.hits)
+    .then(res => res.data)
     .catch(err => err);
 };
